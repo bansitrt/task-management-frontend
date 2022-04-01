90f8680910bfda278601b20c5febc227dfb164fd
bansitrt <bansitrt@gamil.com>
2022-04-01 17:45:45 +0530

    Updates

---
+++ precache-manifest.649f42732c93e07d001074c8935a260d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf2f2c5a5572f8bcd1bba084e7315b3",
+    "url": "/task-management-frontend/index.html"
+  },
+  {
+    "revision": "feb4a480955f26e735e0",
+    "url": "/task-management-frontend/static/css/main.21e1db94.chunk.css"
+  },
+  {
+    "revision": "03cfedeefd08a3e9663f",
+    "url": "/task-management-frontend/static/js/2.304b38f5.chunk.js"
+  },
+  {
+    "revision": "feb4a480955f26e735e0",
+    "url": "/task-management-frontend/static/js/main.bb063eb3.chunk.js"
+  },
+  {
+    "revision": "170cb679b8a6db4918f5",
+    "url": "/task-management-frontend/static/js/runtime~main.961a32b0.js"
+  }
+]);
